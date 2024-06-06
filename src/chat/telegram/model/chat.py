ea2c4907f8816696c3b44c369ce049997d75012d
Milos Marinkovic <milosmns@gmail.com>
2024-06-06 23:42:41 +0200

    Make Telegram models Pydantic-compatible

--- src/chat/telegram/model/chat.py
+++ src/chat/telegram/model/chat.py
@@ -1,24 +1,11 @@
-class Chat:
+from pydantic import BaseModel
+
+
+class Chat(BaseModel):
     """https://core.telegram.org/bots/api#chat"""
     id: int
     type: str
-    title: str | None
-    username: str | None
-    first_name: str | None
-    last_name: str | None
-
-    def __init__(
-        self,
-        id: int,
-        type: str,
-        title: str | None = None,
-        username: str | None = None,
-        first_name: str | None = None,
-        last_name: str | None = None,
-    ):
-        self.id = id
-        self.type = type
-        self.title = title
-        self.username = username
-        self.first_name = first_name
-        self.last_name = last_name
+    title: str | None = None
+    username: str | None = None
+    first_name: str | None = None
+    last_name: str | None = None
--- src/chat/telegram/model/voice.py
+++ src/chat/telegram/model/voice.py
@@ -1,21 +1,10 @@
-class Voice:
+from pydantic import BaseModel
+
+
+class Voice(BaseModel):
     """https://core.telegram.org/bots/api#voice"""
     file_id: str
     file_unique_id: str
     duration: int
-    mime_type: str | None
-    file_size: int | None
-
-    def __init__(
-        self,
-        file_id: str,
-        file_unique_id: str,
-        duration: int,
-        mime_type: str | None = None,
-        file_size: int | None = None
-    ):
-        self.file_id = file_id
-        self.file_unique_id = file_unique_id
-        self.duration = duration
-        self.mime_type = mime_type
-        self.file_size = file_size
+    mime_type: str | None = None
+    file_size: int | None = None
