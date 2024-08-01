33b68b0ee8f157ef05a24bb91e95bb41c4ae4aad
Milos Marinkovic <milosmns@gmail.com>
2024-08-02 00:32:10 +0200

    Add a basic prompt builder, cleanup

--- src/features/web_fetcher.py
+++ src/features/web_fetcher.py
@@ -4,7 +4,7 @@ import time
 import requests
 from requests.exceptions import RequestException, Timeout
 
-from util.config import config, Config
+from util.config import config
 from util.safe_printer_mixin import SafePrinterMixin
 
 PLATFORM = f"{platform.python_implementation()}/{platform.python_version()}"
@@ -16,7 +16,6 @@ class WebFetcher(SafePrinterMixin):
     url: str
     html: str | None = None
     json: dict | None = None
-    __config: Config
 
     def __init__(
         self,
@@ -26,7 +25,6 @@ class WebFetcher(SafePrinterMixin):
     ):
         super().__init__(config.verbose)
         self.url = url
-        self.__config = config
         if auto_fetch_html:
             self.fetch_html()
         if auto_fetch_json:
@@ -35,29 +33,29 @@ class WebFetcher(SafePrinterMixin):
     def fetch_html(self) -> str | None:
         self.html = None  # reset value
         attempts = 0
-        for _ in range(self.__config.web_retries):
+        for _ in range(config.web_retries):
             try:
-                response = requests.get(self.url, headers = HEADERS, timeout = self.__config.web_timeout_s)
+                response = requests.get(self.url, headers = HEADERS, timeout = config.web_timeout_s)
                 response.raise_for_status()
                 self.html = response.text
             except (RequestException, Timeout) as e:
                 attempts += 1
-                attempts_left = self.__config.web_retries - attempts + 1
+                attempts_left = config.web_retries - attempts + 1
                 self.sprint(f"Error fetching HTML content: {e}. Retries left: {attempts_left}.")
-                time.sleep(self.__config.web_retry_delay_s)
+                time.sleep(config.web_retry_delay_s)
         return self.html
 
     def fetch_json(self) -> dict | None:
         self.json = None
         attempts = 0
-        for _ in range(self.__config.web_retries):
+        for _ in range(config.web_retries):
             try:
-                response = requests.get(self.url, headers = HEADERS, timeout = self.__config.web_timeout_s)
+                response = requests.get(self.url, headers = HEADERS, timeout = config.web_timeout_s)
                 response.raise_for_status()
                 self.json = response.json()
             except (RequestException, Timeout) as e:
                 attempts += 1
-                attempts_left = self.__config.web_retries - attempts + 1
+                attempts_left = config.web_retries - attempts + 1
                 self.sprint(f"Error fetching JSON content: {e}. Retries left: {attempts_left}.")
-                time.sleep(self.__config.web_retry_delay_s)
+                time.sleep(config.web_retry_delay_s)
         return self.json
