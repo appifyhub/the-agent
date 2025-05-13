0a756aafaaec6c415440fd0bc3f9659e2f259d2e
Milos Marinkovic <milosmns@gmail.com>
2025-05-13 16:47:47 +0200

    Remove progress updating feature (it was too annoying)

--- src/features/chat/telegram/telegram_progress_notifier.py
+++ src/features/chat/telegram/telegram_progress_notifier.py
@@ -2,15 +2,8 @@ import random
 import time
 from threading import Thread, Lock, Event
 
-from langchain_anthropic import ChatAnthropic
-from langchain_core.language_models import BaseChatModel
-from langchain_core.messages import BaseMessage, SystemMessage, HumanMessage, AIMessage
-from pydantic import SecretStr
-
 from db.schema.chat_config import ChatConfig
-from features.ai_tools.external_ai_tool_library import CLAUDE_3_5_HAIKU
 from features.chat.telegram.sdk.telegram_bot_sdk import TelegramBotSDK
-from features.prompting import prompt_library
 from util.config import config
 from util.safe_printer_mixin import SafePrinterMixin
 
@@ -35,8 +28,6 @@ class TelegramProgressNotifier(SafePrinterMixin):
     __last_text_update_time: float
     __next_reaction_index: int
     __total_cycles: int
-    __llm: BaseChatModel
-    __llm_input: list[BaseMessage]
     __lock: Lock
     __thread: Thread | None
     __signal: Event
@@ -63,21 +54,6 @@ class TelegramProgressNotifier(SafePrinterMixin):
         self.__signal = Event()
         self.__reaction_interval_s = reaction_interval_s
         self.__text_update_interval_s = text_update_interval_s
-        # noinspection PyArgumentList
-        self.__llm = ChatAnthropic(
-            model_name = CLAUDE_3_5_HAIKU.id,
-            temperature = 0.5,
-            max_tokens = 500,
-            timeout = float(config.web_timeout_s),
-            max_retries = config.web_retries,
-            api_key = SecretStr(str(config.anthropic_token)),
-        )
-        prompt = prompt_library.translator_on_response(
-            base_prompt = prompt_library.announcer_event_telegram,
-            language_name = self.__chat_config.language_name,
-            language_iso_code = self.__chat_config.language_iso_code,
-        )
-        self.__llm_input = [SystemMessage(prompt)]
         self.__bot_sdk = bot_sdk
         if auto_start:
             self.start()
@@ -85,10 +61,13 @@ class TelegramProgressNotifier(SafePrinterMixin):
 
     def start(self):
         self.sprint("Acquiring start lock...")
+        if self.__thread and self.__thread.is_alive():
+            self.sprint(f"  Thread {self.__thread.name} is already running (before lock)")
+            return
         with self.__lock:
             self.sprint("  Acquired")
             if self.__thread and self.__thread.is_alive():
-                self.sprint(f"  Thread {self.__thread.name} is already running")
+                self.sprint(f"  Thread {self.__thread.name} is already running (after lock)")
                 return
             self.sprint("  Starting a new thread...")
             self.__signal.clear()
@@ -99,12 +78,15 @@ class TelegramProgressNotifier(SafePrinterMixin):
 
     def stop(self):
         self.sprint("Acquiring stop lock...")
+        if not self.__thread:
+            self.sprint("  No thread running (before lock)")
+            return
         with self.__lock:
             self.sprint("  Acquired")
             self.sprint(f"  Stopping thread {self.__thread.name}...")
             self.__signal.set()
             if not self.__thread:
-                self.sprint("  No thread running")
+                self.sprint("  No thread running (after lock)")
                 return
             self.__thread.join(timeout = 1)  # wait for up to 1 second and proceed anyway
             if self.__thread.is_alive():
@@ -129,8 +111,11 @@ class TelegramProgressNotifier(SafePrinterMixin):
             text_update_elapsed_s = current_time_s - self.__last_text_update_time
 
             if text_update_elapsed_s >= float(self.__text_update_interval_s):
-                # check if a message update is needed
-                self.__send_message()
+                # it takes a long time, let's track it
+                elapsed_time_total_s = time.time() - self.__last_text_update_time
+                elapsed_time_m, elapsed_time_s = divmod(int(elapsed_time_total_s), 60)
+                elapse_time_text = f"{elapsed_time_m} min[s] {elapsed_time_s} sec[s]"
+                self.sprint(f"  Text update interval: {elapse_time_text}")
                 self.__last_text_update_time = current_time_s
             elif reaction_elapsed_s >= float(self.__reaction_interval_s):
                 # check if reaction update is needed
@@ -163,32 +148,3 @@ class TelegramProgressNotifier(SafePrinterMixin):
             self.__bot_sdk.set_reaction(self.__chat_config.chat_id, self.__message_id, next_reaction)
         except Exception as e:
             self.sprint(f"Failed to send reaction: {e}")
-
-    def __send_message(self):
-        self.sprint("Time for a message update")
-        self.__set_chat_status(is_long = True)
-        try:
-            # prepare the message contents
-            elapsed_time_total_s = time.time() - self.__last_text_update_time
-            elapsed_time_m, elapsed_time_s = divmod(int(elapsed_time_total_s), 60)
-            elapse_time_text = f"{elapsed_time_m} min[s] {elapsed_time_s} sec[s]"
-            text_update = f"""
-                [To the user, your buddy]
-                Your requested operation is in it's work cycle number-{self.__total_cycles}.
-                You've been waiting {elapse_time_text} already, and I know mate - it really sucks.
-                I hope your request won't take much longer to complete. I apologize.
-                Wait a while longer and I'll get back to you!
-            """.strip()
-
-            # fetch the actual message viable for sending
-            messages = self.__llm_input.copy()
-            messages.append(HumanMessage(text_update))
-            response = self.__llm.invoke(messages)
-            if not isinstance(response, AIMessage) or not response.content:
-                raise ValueError(f"Got a complex AI response: {response}")
-
-            # get rid of the old reaction and send the update message
-            self.__bot_sdk.set_reaction(self.__chat_config.chat_id, self.__message_id, None)
-            self.__bot_sdk.send_text_message(self.__chat_config.chat_id, str(response.content))
-        except Exception as e:
-            self.sprint("Failed to send message", e)
--- test/features/chat/telegram/test_telegram_progress_notifier.py
+++ test/features/chat/telegram/test_telegram_progress_notifier.py
@@ -7,6 +7,10 @@ from features.chat.telegram.telegram_progress_notifier import TelegramProgressNo
 
 
 class TelegramProgressNotifierTest(unittest.TestCase):
+    chat_config: ChatConfig
+    message_id: str
+    mock_bot_sdk: Mock
+    notifier: TelegramProgressNotifier
 
     def setUp(self):
         self.chat_config = ChatConfig(
@@ -20,16 +24,6 @@ class TelegramProgressNotifierTest(unittest.TestCase):
         self.message_id = "test_message_id"
         self.mock_bot_sdk = Mock(spec = TelegramBotSDK)
         self.mock_bot_sdk.api = Mock(spec = TelegramBotAPI)
-
-        self.mock_chat_anthropic = Mock()
-        self.mock_chat_anthropic.invoke.return_value = Mock(content = "Mocked AI response")
-
-        self.chat_anthropic_patcher = patch(
-            "features.chat.telegram.telegram_progress_notifier.ChatAnthropic",
-            return_value = self.mock_chat_anthropic
-        )
-        self.chat_anthropic_patcher.start()
-
         self.notifier = TelegramProgressNotifier(
             chat_config = self.chat_config,
             message_id = self.message_id,
@@ -37,14 +31,10 @@ class TelegramProgressNotifierTest(unittest.TestCase):
             auto_start = False
         )
 
-    def tearDown(self):
-        self.chat_anthropic_patcher.stop()
-
     # noinspection PyUnresolvedReferences
     def test_init(self):
         self.assertEqual(self.notifier._TelegramProgressNotifier__chat_config, self.chat_config)
         self.assertEqual(self.notifier._TelegramProgressNotifier__message_id, self.message_id)
-        self.assertIsNotNone(self.notifier._TelegramProgressNotifier__llm)
 
     @patch("features.chat.telegram.telegram_progress_notifier.Thread")
     def test_start(self, mock_thread):
@@ -65,33 +55,3 @@ class TelegramProgressNotifierTest(unittest.TestCase):
         self.notifier._TelegramProgressNotifier__send_reaction()
         self.mock_bot_sdk.set_status_typing.assert_called_once_with(self.chat_config.chat_id)
         self.mock_bot_sdk.set_reaction.assert_called_once()
-
-    @patch("features.chat.telegram.telegram_progress_notifier.time.time")
-    def test_send_message(self, mock_time):
-        mock_time.return_value = 100
-
-        # Call the method
-        # noinspection PyUnresolvedReferences
-        self.notifier._TelegramProgressNotifier__send_message()
-
-        # Collect information about method calls
-        bot_sdk_calls = self.mock_bot_sdk.method_calls
-        chat_anthropic_calls = self.mock_chat_anthropic.method_calls
-
-        # Assert on the collected information
-        self.assertGreater(len(bot_sdk_calls), 0, "No methods were called on TelegramBotAPI")
-        self.assertEqual(len(chat_anthropic_calls), 1, "Expected one call to ChatAnthropic")
-
-        # Check specific method calls
-        self.mock_bot_sdk.set_status_uploading_image.assert_called_once_with(self.chat_config.chat_id)
-        self.mock_chat_anthropic.invoke.assert_called_once()
-        self.mock_bot_sdk.send_text_message.assert_not_called()
-
-        # If we want to check the content of the ChatAnthropic call:
-        chat_anthropic_call = chat_anthropic_calls[0]
-        self.assertEqual(chat_anthropic_call[0], 'invoke', "Unexpected method called on ChatAnthropic")
-        self.assertIsInstance(chat_anthropic_call[1][0], list, "Expected first argument to invoke to be a list")
-
-        # Assert that at least one method was called overall
-        total_calls = len(bot_sdk_calls) + len(chat_anthropic_calls)
-        self.assertGreater(total_calls, 0, "No methods were called on any mocked object")
