12296cbf9ecc94e7be215b4a521d9690c0264475
Milos Marinkovic <milosmns@gmail.com>
2025-12-17 12:18:01 +0100

    Make WA forwarding context optional

--- src/features/chat/whatsapp/model/context.py
+++ src/features/chat/whatsapp/model/context.py
@@ -3,5 +3,5 @@ from pydantic import BaseModel, Field
 
 class Context(BaseModel):
     """https://developers.facebook.com/docs/whatsapp/cloud-api/webhooks/reference/messages"""
-    from_: str = Field(alias = "from")
-    id: str
+    from_: str | None = Field(default = None, alias = "from")
+    id: str | None = None
